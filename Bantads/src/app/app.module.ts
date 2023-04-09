36a1fafb1bd023c664d0c4a0abeb18e00b402678
João Vitor Andrade da Silva <andradeclashofclans17@gmail.com>
2023-04-09 16:59:18 -0300

    Tela Inicial

--- Bantads/src/app/app.module.ts
+++ Bantads/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuFormComponent } from './menu-form/menu-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuFormComponent
   ],
   imports: [
     BrowserModule,
