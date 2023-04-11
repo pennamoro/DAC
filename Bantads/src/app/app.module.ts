6d5bf82429c8259410000afd251fb99db356e8da
João Vitor Andrade da Silva <andradeclashofclans17@gmail.com>
2023-04-11 19:05:28 -0300

    Roteamento e User check no header

--- Bantads/src/app/app.module.ts
+++ Bantads/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
@@ -9,6 +10,7 @@ import { FormComponent } from '../components/user-form/form.component';
 import { MenuTemplateComponent } from '../components/menu-frame/menu-template.component';
 import { MenuExtratoComponent } from '../components/menu-components/menu-extrato/menu-extrato.component';
 import { UserCheckHeaderComponent } from '../components/user-check-header/user-check-header.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { UserCheckHeaderComponent } from '../components/user-check-header/user-c
     FormComponent,
     MenuTemplateComponent,
     MenuExtratoComponent,
-    UserCheckHeaderComponent
+    UserCheckHeaderComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
